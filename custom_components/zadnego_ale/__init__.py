db4a809c79f5bdccea3fe475433380e77d1b49b1
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-04-03 19:53:54 +0200

    Remove import config

--- custom_components/zadnego_ale/__init__.py
+++ custom_components/zadnego_ale/__init__.py
@@ -8,7 +8,7 @@ import async_timeout
 from zadnegoale import ApiError, ZadnegoAle
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
