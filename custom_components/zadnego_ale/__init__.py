53e8eb91ebb0050695690db10deca8d84df4a503
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-27 09:49:36 +0200

    Fix flake8 error

--- custom_components/zadnego_ale/__init__.py
+++ custom_components/zadnego_ale/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 import asyncio
 import logging
-from typing import Any
 
 from aiohttp import ClientSession
 from aiohttp.client_exceptions import ClientConnectorError
