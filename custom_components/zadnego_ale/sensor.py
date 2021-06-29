563e2a46f645f2a0438f7e48e0328aba6c75c3cd
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-06-29 15:55:43 +0200

    Fix flake8 error

--- custom_components/zadnego_ale/sensor.py
+++ custom_components/zadnego_ale/sensor.py
@@ -7,7 +7,6 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ATTRIBUTION
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
